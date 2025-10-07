4986e72150c7520bd17dc6544e6913b4a6299148
aidan <aidanbains09@gmail.com>
2025-10-07 14:15:27 -0700

    Added McDoBot

---
+++ work.mcdobot.py
@@ -0,0 +1,7 @@
+# Work - McDoBot
+# Author: Aidan Bains
+# 6 October
+
+# It should accept Yes/yes or No/no
+# as input, and reply appriately depending on the answer:
+# If the user inputs anything else,
